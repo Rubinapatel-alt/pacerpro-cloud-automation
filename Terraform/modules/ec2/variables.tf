f88ed4c7662ba6da803e7fb85c8ab4fbc0eb0da2
Rubinapatel-alt <Rubinapatel65@yahoo.com>
2026-01-27 14:42:47 -0500

    Update variables.tf

--- Terraform/modules/ec2/variables.tf
+++ Terraform/modules/ec2/variables.tf
@@ -1,3 +1,15 @@
 variable "ami" {}
 variable "instance_type" {}
 variable "name" {}
+
+
+
+sns/variables.tf
+
+variable "name" {}
+
+lambda/variables.tf
+
+variable "lambda_zip" {}
+variable "function_name" {}
+variable "ec2_instance_id" {}
